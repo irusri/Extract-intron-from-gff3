9554b49ed76372fe53d3758d14a40f8d00377bcd
Chanaka Mannapperuma <irusri@yahoo.com>
2013-10-30 18:29:22 +0100

    Update extract_intron_gff3_from_gff3.py

--- scripts/extract_intron_gff3_from_gff3.py
+++ scripts/extract_intron_gff3_from_gff3.py
@@ -21,7 +21,7 @@ def instert_introns_to_gff3(gff_filename, output_gff3_filename):
         gene_rec = gene_tree[gene_id]["gene"]
         gene_start = int(str(gene_tree[gene_id]['gene']).split(",")[3].strip(" "))
         gene_end = int(str(gene_tree[gene_id]['gene']).split(",")[4].strip(" "))
-        # Write the GFF record
+         # Write the GFF record
         gff_out.write(gene_rec)
         # Write out the mRNAs, their exons, and then
         # input the introns
@@ -30,16 +30,64 @@ def instert_introns_to_gff3(gff_filename, output_gff3_filename):
             gff_out.write(curr_mRNA["record"])
             # Write out the exons
             curr_exons = gene_tree[gene_id]["mRNAs"][mRNA_id]["exons"]
+	    #curr_cds= gene_tree[gene_id]["mRNAs"][mRNA_id]["CDSs"]	
             for exon in curr_exons:
                 gff_out.write(curr_exons[exon]["record"])
+		#gff_out.write(curr_cds[cds]["record"])
         # Now output the introns
         for isoform in gene_obj.isoforms:
+	    #print gene_obj.isoforms
             intron_coords = []
             for first_exon, second_exon in zip(isoform.parts,
-                                               isoform.parts[1::1]):
+                                               isoform.parts):
+                final_exon_end=0
+                first_exon_start=0
+                if(len(intron_coords)==len(isoform.parts)-1):
+                    final_exon_end=second_exon.end
+                if(len(isoform.parts)==1):
+                    first_exon_start=first_exon.start
+                #print first_exon
+                
+                if(first_exon_start>1 and gene_start==1):
+                    intron_start=1
+                    intron_end = first_exon_start - 1
+                    
+                elif(gene_end>final_exon_end and final_exon_end!=0):
+                    #print str(isoform.parts[len(isoform.parts)-1]).split(",")
+                    print first_exon_start
+                    intron_start=final_exon_end+1
+                    intron_end=gene_end
+                else:   
+                    intron_start = first_exon.end + 1
+                    intron_end = second_exon.start - 1
+                     
+                if intron_start >= intron_end:
+                    continue
+                intron_coords.append((intron_start, intron_end))
+                # Create record for this intron
+                intron_id = "%s:%d-%d:%s" %(gene_obj.chrom,
+                                            intron_start,
+                                            intron_end,gene_obj.strand)
+                intron_rec = \
+                    gff_utils.GFF(gene_obj.chrom, gene_rec.source, "intron",
+                                  intron_start, intron_end,".",gene_obj.strand,
+                                  attributes={"ID": [gene_obj.label],
+                                              "Parent": [isoform.label]})
+                gff_out.write(intron_rec)
+
+
+            
+            for first_exon, second_exon in zip(isoform.parts,
+                                               isoform.parts[1::1]): 
                 # Intron start coordinate is the coordinate right after
                 # the end of the first exon, intron end coordinate is the
                 # coordinate just before the beginning of the second exon
+                #print "test"
+		        #print "s"
+                #else:    
+                #    intron_start = first_exon.end + 1
+                #    intron_end = second_exon.start - 1
+
                 intron_start = first_exon.end + 1
                 intron_end = second_exon.start - 1
                 if intron_start >= intron_end:
@@ -48,8 +96,7 @@ def instert_introns_to_gff3(gff_filename, output_gff3_filename):
                 # Create record for this intron
                 intron_id = "%s:%d-%d:%s" %(gene_obj.chrom,
                                             intron_start,
-                                            intron_end,
-                                            gene_obj.strand)
+                                            intron_end,gene_obj.strand)
                 intron_rec = \
                     gff_utils.GFF(gene_obj.chrom, gene_rec.source, "intron",
                                   intron_start, intron_end,".",gene_obj.strand,
